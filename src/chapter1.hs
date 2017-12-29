65ad625eabaea68814b5a034dc33ed892e69a5a0
Dmitrii Skurikhin <skurikhin@tochka.com>
2017-12-29 15:40:43 +0500

    added 1.4

--- src/chapter1.hs
+++ src/chapter1.hs
@@ -5,5 +5,19 @@ beside :: Nat -> Nat -> Bool
 beside x y = succ x == y || succ y == x
 
 -- 2
-beside2 :: Nat -> Nat -> Bool
-beside2 distance x y = 
+beside2 :: Int -> Int -> Int -> Bool
+beside2 distance x y = (abs $ x - y) == distance
+
+-- 3 TODO
+
+
+-- 4
+pow :: Nat -> Nat -> Nat
+pow 0 _ = 0
+pow _ 0 = 1
+pow x degree = x * pow x (degree - 1)
+
+-- 5 TODO
+
+
+-- 6
