13e914e2dd23125da74d4f08e767a36c7d804860
Dmitrii Skurikhin <skurikhin@tochka.com>
2018-01-09 12:29:10 +0500

    added binary tree map

--- src/chapter11.hs
+++ src/chapter11.hs
@@ -1,18 +1,23 @@
 -- Binary tree
-data BinaryTree a = Leaf | Node (BinaryTree a) a (BinaryTree a)
+data BinaryTree a =
+    Leaf | Node
+    { left:: BinaryTree a
+    , index:: a
+    , right:: BinaryTree a
+    }
     deriving (Eq, Ord, Show)
 
 insert' :: Ord a => a -> BinaryTree a -> BinaryTree a
-insert' b Leaf = Node Leaf b Leaf
-insert' b (Node left a right)
-    | b == a = Node left a right
-    | b < a = Node(insert' b left) a right
-    | b > a = Node left a (insert' b right)
+insert' node Leaf = Node Leaf node Leaf
+insert' node (Node left index right)
+    | node == index = Node left index right
+    | node < index = Node(insert' node left) index right
+    | node > index = Node left index (insert' node right)
 
 mapTree :: (a -> b) -> BinaryTree a -> BinaryTree b
 mapTree _ Leaf = Leaf
-mapTree f (Node left a right) =
-    Node undefined undefined undefined
+mapTree f (Node left index right) =
+    Node (mapTree f left) (f index) (mapTree f right)
 
 testTree' :: BinaryTree Integer
 testTree' =
