687818bdb049bfc10dcedeeb093eeca6db679452
Dmitrii Skurikhin <dmitrii.sk@gmail.com>
2018-01-18 07:41:24 +0500

    added one Functot instance

--- src/Chapter16.hs
+++ src/Chapter16.hs
@@ -1,3 +1,11 @@
-module Chapter16 (Identity) where
+module Chapter16 (Pair (Pair)) where
 
 newtype Identity a = Identity a
+
+instance Functor Identity where
+    fmap f (Identity x) = Identity $ f x
+
+data Pair a = Pair a a deriving (Eq, Show)
+
+instance Functor Pair where
+    fmap f (Pair x y) = Pair (f x) (f y)
--- test/Chapter16Spec.hs
+++ test/Chapter16Spec.hs
@@ -2,13 +2,15 @@ module Chapter16Spec where
 
 import Test.Hspec
 import Test.QuickCheck
-import Test.QuickCheck.Function
-import Chapter16 (Identity)
+import Chapter16 (Pair (Pair))
+
+functorCompose' :: (Eq (f c), Functor f) => f a -> (a -> b) -> (b -> c) -> Bool
+functorCompose' x f g = (fmap (g . f) x) == (fmap g . fmap f $ x)
 
 chapter16spec :: SpecWith ()
 chapter16spec =
     describe "Chapter16 Functor" $ do
         context "Functor instances" $ do
-            it "Identity compose works" $ property $ do
-                functorCompose' :: IntFC
+            it "Pair works" $ do
+                fmap ((+1) . (*3)) (Pair 1 3) `shouldBe` (Pair 4 10)
 
