882d7c75dd48468c54aeedcc12aeefa3e31905a3
Dmitrii Skurikhin <skurikhin@tochka.com>
2018-01-19 14:39:28 +0500

    16.10 Functor instances

--- src/Chapter16.hs
+++ src/Chapter16.hs
@@ -1,6 +1,6 @@
-module Chapter16 (Pair (Pair)) where
+module Chapter16 where
 
-newtype Identity a = Identity a
+newtype Identity a = Identity a deriving (Eq, Show)
 
 instance Functor Identity where
     fmap f (Identity x) = Identity $ f x
@@ -9,3 +9,28 @@ data Pair a = Pair a a deriving (Eq, Show)
 
 instance Functor Pair where
     fmap f (Pair x y) = Pair (f x) (f y)
+
+data Two a b = Two a b deriving (Eq, Show)
+
+instance Functor (Two a) where
+    fmap f (Two x y) = Two x (f y)
+
+data Three a b c = Three a b c deriving (Eq, Show)
+
+instance Functor (Three a b) where
+    fmap f (Three x y z) = Three x y (f z)
+
+data Three' a b = Three' a b b deriving (Eq, Show)
+
+instance Functor (Three' a) where
+    fmap f (Three' x y z) = Three' x (f y) (f z)
+
+data Four a b c d = Four a b c d deriving (Eq, Show)
+
+instance Functor (Four a b c) where
+    fmap f (Four a b c d) = Four a b c (f d)
+
+data Four' a b = Four' a a a b deriving (Eq, Show)
+
+instance Functor (Four' a) where
+    fmap f (Four' a b c d) = Four' a b c (f d)
--- test/Chapter16Spec.hs
+++ test/Chapter16Spec.hs
@@ -2,15 +2,51 @@ module Chapter16Spec where
 
 import Test.Hspec
 import Test.QuickCheck
-import Chapter16 (Pair (Pair))
+import Chapter16
 
 functorCompose' :: (Eq (f c), Functor f) => f a -> (a -> b) -> (b -> c) -> Bool
 functorCompose' x f g = (fmap (g . f) x) == (fmap g . fmap f $ x)
+
+functorId' :: (Eq (f a), Functor f) => f a -> Bool
+functorId' x = (fmap id x) == (id x)
 
 chapter16spec :: SpecWith ()
 chapter16spec =
     describe "Chapter16 Functor" $ do
         context "Functor instances" $ do
-            it "Pair works" $ do
-                fmap ((+1) . (*3)) (Pair 1 3) `shouldBe` (Pair 4 10)
+            it "Identity id" $ property $ do
+                functorId' (Identity 2)
+
+            it "Identity compose" $ property $ do
+                functorCompose' (Identity 2) (+3) (*3)
+
+            it "Pair id" $ property $ do
+                functorId' (Pair 1 3)
+
+            it "Pair compose" $ property $ do
+                functorCompose' (Pair 1 3) (+1) (*3)
+
+            it "Two id" $ do
+                fmap id (Two 'a' 1) `shouldBe` id (Two 'a' 1)
+
+            it "Two compose" $ do
+                fmap ((+1) . (*3)) (Two 'a' 1) `shouldBe` (Two 'a' 4)
+
+            it "Three id" $ do
+                fmap id (Three "1" 'a' (3 :: Int)) `shouldBe` id (Three "1" 'a' (3 :: Int))
+
+            it "Three compose" $ do
+                fmap ((+1) . (*3)) (Two 'a' 1) `shouldBe` (Two 'a' 4)
+
+            it "Three' id" $ do
+                fmap id (Three' "1" (2 :: Int) (3 :: Int)) `shouldBe` id (Three' "1" (2 :: Int) (3 :: Int))
+
+            it "Three' compose" $ do
+                fmap ((+1) . (*3)) (Three' "1" 2 3) `shouldBe` (Three' "1" 7 10)
+
+            it "Four' id" $ do
+                functorId' (Four' 1 3 7 'a')
+
+            it "Four' compose" $ do
+                functorCompose' (Four' 1 3 2 4) (*2) (+3)
 
