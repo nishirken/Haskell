de9a30ba16b12b96e6537f93e45c8388375265d9
Дмитрий Скурихин <dmitry.skurihin@monopoly.su>
2019-07-11 19:46:58 +0300

    add QQ ini configs

--- src/Parse.hs
+++ src/Parse.hs
@@ -1,2 +1,26 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+
 module Parse where
 
+import Control.Applicative
+import Data.ByteString (ByteString)
+import Data.Char (isAlpha)
+import Data.Text (Text)
+import qualified Data.Map as M
+import qualified Data.Text.IO as TextIO
+import Text.RawString.QQ
+
+sectionEx' :: ByteString
+sectionEx' = [r|
+  ; ignore me
+  [states]
+  Chris=Texas
+  |]
+
+sectionEx'' :: ByteString
+sectionEx'' = [r|
+  ; comment
+  [section] host=wikipedia.org alias=claw
+  [whatisit] red=intoothandclaw
+  |]
