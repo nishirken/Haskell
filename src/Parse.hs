2a2f298379db9489e580b2bd085c9c7aeb31f7bc
Дмитрий Скурихин <dmitry.skurihin@monopoly.su>
2019-07-12 14:16:11 +0300

    add ini parser

--- src/Parse.hs
+++ src/Parse.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE QuasiQuotes #-}
 
 module Parse where
 
@@ -10,7 +9,13 @@ import Data.Char (isAlpha)
 import Data.Text (Text)
 import qualified Data.Map as M
 import qualified Data.Text.IO as TextIO
-import Text.RawString.QQ
+
+data IniSection = IniSection
+  { _headers :: [Header]
+  , _assignments :: Assignments
+  } deriving (Eq, Show)
+
+type IniConfig = [IniSection]
 
 newtype Header = Header String deriving (Eq, Ord, Show)
 
@@ -18,7 +23,10 @@ parseBracketPair :: Parser a -> Parser a
 parseBracketPair p = char '[' *> p <* char ']'
 
 parseHeader :: Parser Header
-parseHeader = parseBracketPair (Header <$> some letter)
+parseHeader = do
+  header <- parseBracketPair (Header <$> some (alphaNum <|> char '.'))
+  skipWhitespace
+  pure header
 
 type Name = String
 type Value = String
@@ -28,43 +36,27 @@ parseAssignment :: Parser (Name, Value)
 parseAssignment = do
   name <- some letter
   char '='
-  val <- some (noneOf "\n")
-  skipEOL
+  val <- some (noneOf "\t \n")
+  skipWhitespace
   pure (name, val)
-
-skipEOL :: Parser ()
-skipEOL = skipMany (oneOf "\n")
-
-commentEx :: ByteString
-commentEx =
-  "; last modified 1 April\
-  \ 2001 by John Doe"
-
-commentEx' :: ByteString
-commentEx' =
-  "; blah\n; woot\n  \n;hah"
 
 skipComments :: Parser ()
 skipComments =
   skipMany $ do
     char ';' <|> char '#'
     skipMany (noneOf "\n")
-    skipEOL
 
-sectionEx :: ByteString
-sectionEx =
-  "; ignore me\n[states]\nChris=Texas"
+skipWhitespace :: Parser ()
+skipWhitespace = skipMany (char '\t' <|> char ' ' <|> char '\n')
 
-sectionEx' :: ByteString
-sectionEx' = [r|
-  ; ignore me
-  [states]
-  Chris=Texas
-  |]
+parseSection :: Parser IniSection
+parseSection = do
+  skipWhitespace
+  skipComments
+  skipWhitespace
+  headers <- some parseHeader
+  assignments <- some parseAssignment
+  pure $ IniSection headers (M.fromList assignments)
 
-sectionEx'' :: ByteString
-sectionEx'' = [r|
-  ; comment
-  [section] host=wikipedia.org alias=claw
-  [whatisit] red=intoothandclaw
-  |]
+parseIni :: Parser IniConfig
+parseIni = some parseSection
--- test/ParseSpec.hs
+++ test/ParseSpec.hs
@@ -1,11 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
 
 module ParseSpec where
 
 import Test.Hspec (context, describe, it, Spec, shouldBe)
 import Parse
 import Data.ByteString (ByteString)
-import Text.Trifecta (parseByteString, Result (..), some)
+import Text.Trifecta (parseByteString, parseTest, Result (..), some)
+import Text.RawString.QQ
+import qualified Data.Map as M
 
 instance Eq a => Eq (Result a) where
   (Success x) == (Success y) = x == y
@@ -19,3 +22,53 @@ parseSpec = describe "Parser" $ do
   it "Assignment" $
     parseByteString (some parseAssignment) mempty ("key=value\n\n\ntest=data" :: ByteString) `shouldBe`
       (Success $ [("key", "value"), ("test", "data")])
+
+  it "Section" $ do
+    let
+      sectionEx :: ByteString
+      sectionEx = "; ignore me  \n  [states]\n  [states22.0]   \n  Chris=Texas  \n\n"
+      expect :: IniSection
+      expect = IniSection [Header "states", Header "states22.0"] $ M.fromList [("Chris", "Texas")]
+    parseByteString parseSection mempty sectionEx `shouldBe` Success expect
+
+  context "Entire test" $ do
+    it "First" $ do
+      let
+        sectionEx' :: ByteString
+        sectionEx' = [r|
+
+
+          ; ignore me
+          [states]
+          Chris=Texas
+          |]
+        expect :: IniConfig
+        expect = [IniSection [Header "states"] $ M.fromList [("Chris", "Texas")]]
+      parseByteString parseIni mempty sectionEx' `shouldBe` Success expect
+
+    it "Second" $ do
+      let
+        sectionEx'' :: ByteString
+        sectionEx'' = [r|
+
+          ; comment
+          [section]
+          host=wikipedia.org
+          alias=claw
+
+
+          # unix comment
+          [whatisit]
+          [subtitle]
+          red=intoothandclaw
+          blue=white
+
+          |]
+        expect :: IniConfig
+        expect =
+          [ IniSection [Header "section"] $
+              M.fromList [("host", "wikipedia.org"), ("alias", "claw")]
+          , IniSection [Header "whatisit", Header "subtitle"] $
+              M.fromList [("red", "intoothandclaw"), ("blue", "white")]
+          ]
+      parseByteString parseIni mempty sectionEx'' `shouldBe` Success expect
